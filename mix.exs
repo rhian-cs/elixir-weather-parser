71084f9fdbb243c7c94267a6a03c0ad5e398d723
rhian-cs <rhian.luis.cs+github@gmail.com>
2022-04-27 10:45:32 -0300

    Add coveralls

--- mix.exs
+++ mix.exs
@@ -9,6 +9,13 @@ defmodule WeatherParser.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
       deps: deps()
     ]
   end
@@ -27,7 +34,8 @@ defmodule WeatherParser.MixProject do
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:httpoison, "~> 1.0.0"},
       {:ex_doc, "~> 0.27"},
-      {:earmark, "~> 1.4.24"}
+      {:earmark, "~> 1.4.24"},
+      {:excoveralls, "~> 0.5.5", only: :test}
     ]
   end
 
